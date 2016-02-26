24977142e6c9e450947e87c2207aa06809e7bf5a
Ovsyannikov Aleksey <insanus-mokrassar@yandex.ru>
2016-02-26 18:55:47 +0600

    ready the skeleton of app

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
-angular.module('configApp', ['ngRoute', 'config.services','config.filters'])
+var appModule = angular.module('configApp', ['ngRoute', 'config.services','config.filters'])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
-      .when('/list', {template: 'templates/list.html', controller: ListCtrl})
-      .when('/new', {template: 'templates/edit.html', controller: NewCtrl})
-      .when('/edit/:id', {template: 'templates/edit.html', controller: EditCtrl})
-      .otherwise({redirectTo: '/list'});
+      .when('/main', {template: 'templates/main.html', controller: MainCtrl})
+      .otherwise({redirectTo: '/main'});
   }]);
+
+appModule.controller('MainCtrl', MainCtrl);
