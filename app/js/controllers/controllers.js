7a5ac4897c607de22876cfe1209c3894fcc804b5
Ovsyannikov Aleksey <insanus-mokrassar@yandex.ru>
2016-03-01 19:58:03 +0600

    add actor template and write actor controller

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -1,6 +1,26 @@
 'use strict';
 
-function MainCtrl() {
+function MainCtrl($scope) {
   var me = this;
-  me.message = 'Hi!';
+  me.actors = [];
+  me.actors.push(
+      {
+        title: 'First added actor',
+        description: 'It is first actor, which have description and can be displayed',
+        addingDate: new Date(),
+        liveTime: new Date(),
+        status: 'warning'
+      },
+      {
+        title: 'Second added actor',
+        description: 'It is second actor, which have description and can be displayed',
+        addingDate: new Date(),
+        liveTime: new Date(),
+        starred: true,
+        status: 'normal'
+      }
+  );
+  me.toStar = function (element) {
+    element.starred = true;
+  };
 }
